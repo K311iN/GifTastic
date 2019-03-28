56971c08c74f3932518d87ead29568e47c4747be
Kellin <kellinjoshua@gmail.com>
2019-03-28 01:49:38 -0700

    update gifs.js
    Changed searchImage to search-image

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
     });
 
 
-    $(document).on("click", ".searchImage", function () {
+    $(document).on("click", ".search-image", function () {
 
         var state = $(this).attr("data-state");
 
